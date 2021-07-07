9c3b90e6defc7813e1794ec0b71917129ba8dcfd
Roman <49787417+RKDGT@users.noreply.github.com>
2021-07-07 07:51:58 -0400

    Update events.cljs

--- src/binance/events.cljs
+++ src/binance/events.cljs
@@ -8,11 +8,6 @@
 
 (def default-uri (r/atom "https://www.binance.com/api/v3/ticker/price"))
 
-(re-frame/reg-cofx
-  :current-data
-  (fn [cofx _data]
-    (assoc cofx :current-data (js/Date.))))
-
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
@@ -20,34 +15,23 @@
 
 (re-frame/reg-event-fx
  :request
- (fn [db _ current]
-    (when (not (= (:filter db) "ALL"))
-     (let [uri (if (not (= current "ALL"))
-                 @default-uri
-                 "")]
-       (do
-              ;; (js/console.log (:filter db))
-              ;; (reset! default-uri (str @default-uri "?symbol=" (:filter db)))
-         {:http-xhrio {:uri uri
-                       :method :get
-                       :timeout 10000
-                       :format (ajax/json-request-format)
-                       :response-format (ajax/json-response-format {:keywords? true})
-                       :on-success [:process-response]
-                       :on-failure [:bad-response]}}))))
-   )
+ (fn []
+   {:http-xhrio {:uri @default-uri
+                 :method :get
+                 :timeout 10000
+                 :format (ajax/json-request-format)
+                 :response-format (ajax/json-response-format {:keywords? true})
+                 :on-success [:process-response]
+                 :on-failure [:bad-response]}}))
 
 (re-frame/reg-event-db
-  :process-response
-  (fn [db [_ response]]
-    (assoc db :current-data (js->clj response))))
+ :process-response
+ (fn [db [_ response]]
+   (-> db
+    (assoc :current-data (js->clj response))
+    (assoc :time (js/Date.)))))
 
 (re-frame/reg-event-db
-  :bad-response
-  (fn [_ [_ response]]
-    (println response)))
-
-(re-frame/reg-event-db
- ::filter-event
- (fn [db [_ clicked-numb]]
-   (conj db :filter clicked-numb)))
+ :bad-response
+ (fn [_ [_ response]]
+   (println response)))
